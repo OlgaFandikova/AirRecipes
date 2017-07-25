f78e1f790db9a4a9d3629628a2a92043990ced56
Olga Fandikova <olafan92@gmail.com>
2017-07-25 12:42:16 +0700

    Добавлен лоадер.

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 
+import PageLoader from 'common/components/PageLoader'
 import Header from './header/Header'
 
 
@@ -8,6 +9,7 @@ export default class Layout extends React.Component<{}, {}> {
     render() {
         return (
             <div>
+                <PageLoader />
                 <Header />
                 <div className="container pt-md-30 pt-lg-30 pb-mg-30 pb-lg-30">
                     {this.props.children}
